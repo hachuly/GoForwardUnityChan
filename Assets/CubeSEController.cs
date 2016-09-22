644c05fe8799282a35c0e1aa2d5ff798e33a3859
logbeam <desu_taratyann@yahoo.co.jp>
2016-09-23 02:26:07 +0900

    GoForwardUnityChanプロジェクト　ユニティちゃんが箱を飛び越えます
    課題に記載された、箱がUnityChan以外の物にあたると音がする機能を実装しました。

---
+++ Assets/CubeSEController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeSEController : MonoBehaviour {
+
+    private AudioSource SE;
+    private bool BlockSETrigger = true;
+
+	// Use this for initialization
+	void Start () {
+	   SE = GetComponent<AudioSource>();
+	}
+
+    void OnCollisionEnter2D(Collision2D other){
+        if(BlockSETrigger){
+            SE.PlayOneShot(SE.clip);
+            BlockSETrigger = false;
+        }
+
+    }
+
+}
---
+++ Assets/UIController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+//Sceneを使う場合
+using UnityEngine.SceneManagement;
+
+public class UIController : MonoBehaviour {
+
+    private GameObject gameOverText;
+    private GameObject runLengthText;
+    private float len = 0;
+    private float speed = 0.03f;
+    private bool isGameOver = false;
+
+	// Use this for initialization
+	void Start () {
+	   this.gameOverText = GameObject.Find("GameOver");
+       this.runLengthText = GameObject.Find("RunLength");
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+        //走った距離の表示
+	   if(this.isGameOver == false){
+            this.len += this.speed;
+            this.runLengthText.GetComponent<Text>().text = "Distance:  " + len.ToString("F2") + "m";
+       }
+        if(this.isGameOver){
+            if(Input.GetMouseButtonDown(0)){
+                    SceneManager.LoadScene("GameScene");
+                }
+            }
+	}
+
+    public void GameOver(){
+        this.gameOverText.GetComponent<Text>().text = "GameOver";
+        this.isGameOver = true;
+    }
+}
